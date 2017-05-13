f8ad2bc11862fa330b2901b482664e70d794ee85
Akira Kitauchi <kitauchi@gmail.com>
2017-05-13 21:52:28 +0900

    display notification on startup

--- ReconnectWifiOnWakeup.app/Contents/MacOS/ReconnectWifiOnWakeup.sh
+++ ReconnectWifiOnWakeup.app/Contents/MacOS/ReconnectWifiOnWakeup.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+appname='ReconnectWifiOnWakeup'
 dir=$(cd $(dirname $0); pwd)
 airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
 device=$(networksetup -listallhardwareports | grep -A1 '^Hardware Port: Wi-Fi' | grep '^Device:' | head -1 | awk '{print $2}')
 device=${device:-en0}
 
-pgrep -q -f 'awakened.*networksetup' || "$dir/awakened" /bin/sh -c "$airport -I | grep -qi 'airport: off' || (networksetup -setairportpower $device off; sleep 1; networksetup -setairportpower $device on)" &
+if pgrep -q -f 'awakened.*networksetup'; then
+  osascript -e "display notification \"Alreadly started\" with title \"$appname\""
+else
+  "$dir/awakened" /bin/sh -c "$airport -I | grep -qi 'airport: off' || (networksetup -setairportpower $device off; sleep 1; networksetup -setairportpower $device on)" &
+  osascript -e "display notification \"Successfully started on device $device\" with title \"$appname\""
+fi
