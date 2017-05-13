0989978b43135853d0686c75f884c1045ce9a319
Akira Kitauchi <kitauchi@gmail.com>
2017-05-13 22:45:35 +0900

    Fixed typo

--- ReconnectWifiOnWakeup.app/Contents/MacOS/ReconnectWifiOnWakeup.sh
+++ ReconnectWifiOnWakeup.app/Contents/MacOS/ReconnectWifiOnWakeup.sh
@@ -6,7 +6,7 @@ device=$(networksetup -listallhardwareports | grep -A1 '^Hardware Port: Wi-Fi' |
 device=${device:-en0}
 
 if pgrep -q -f 'awakened.*networksetup'; then
-  osascript -e "display notification \"Alreadly started\" with title \"$appname\""
+  osascript -e "display notification \"Already started\" with title \"$appname\""
 else
   "$dir/awakened" /bin/sh -c "$airport -I | grep -qi 'airport: off' || (networksetup -setairportpower $device off; sleep 1; networksetup -setairportpower $device on)" &
   osascript -e "display notification \"Successfully started on device $device\" with title \"$appname\""
