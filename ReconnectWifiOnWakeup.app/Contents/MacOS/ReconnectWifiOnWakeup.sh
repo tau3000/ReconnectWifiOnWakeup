b0241966cef7649c0b544cf021b1dfa9da75a19f
Akira Kitauchi <kitauchi@gmail.com>
2017-05-13 20:51:49 +0900

    Don't reconnect Wi-Fi when Wi-Fi is off

--- ReconnectWifiOnWakeup.app/Contents/MacOS/ReconnectWifiOnWakeup.sh
+++ ReconnectWifiOnWakeup.app/Contents/MacOS/ReconnectWifiOnWakeup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 dir=$(dirname $0)
-pgrep -q -f 'awakened.*networksetup' || $dir/awakened /bin/sh -c 'networksetup -setairportpower en0 off; sleep 1; networksetup -setairportpower en0 on' &
-end
+airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
+
+pgrep -q -f 'awakened.*networksetup' || $dir/awakened /bin/sh -c "$airport -I | grep -qi 'airport: off' || (networksetup -setairportpower en0 off; sleep 1; networksetup -setairportpower en0 on)" &
