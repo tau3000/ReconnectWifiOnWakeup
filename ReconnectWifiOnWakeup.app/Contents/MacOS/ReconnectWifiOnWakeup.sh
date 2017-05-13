b34a50ad76f7aeba0f02cb4067c0664bee80b52e
Akira Kitauchi <kitauchi@gmail.com>
2017-05-13 21:11:53 +0900

    Get the Wi-Fi device name with 'networksetup -listallhardwareports'

--- ReconnectWifiOnWakeup.app/Contents/MacOS/ReconnectWifiOnWakeup.sh
+++ ReconnectWifiOnWakeup.app/Contents/MacOS/ReconnectWifiOnWakeup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-dir=$(dirname $0)
+dir=$(cd $(dirname $0); pwd)
 airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
+device=$(networksetup -listallhardwareports | grep -A1 '^Hardware Port: Wi-Fi' | grep '^Device:' | head -1 | awk '{print $2}')
+device=${device:-en0}
 
-pgrep -q -f 'awakened.*networksetup' || $dir/awakened /bin/sh -c "$airport -I | grep -qi 'airport: off' || (networksetup -setairportpower en0 off; sleep 1; networksetup -setairportpower en0 on)" &
+pgrep -q -f 'awakened.*networksetup' || "$dir/awakened" /bin/sh -c "$airport -I | grep -qi 'airport: off' || (networksetup -setairportpower $device off; sleep 1; networksetup -setairportpower $device on)" &
