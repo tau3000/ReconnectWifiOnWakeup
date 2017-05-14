4d4f15ac38c17993fd97f2b6c098c47acf589ec4
Akira Kitauchi <kitauchi@gmail.com>
2017-05-15 02:13:01 +0900

    Do nothing when clicking notification

--- WiFiReconnect.app/Contents/MacOS/start.sh
+++ WiFiReconnect.app/Contents/MacOS/start.sh
@@ -5,10 +5,11 @@ APPNAME='WiFiReconnect'
 AIRPORT='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
 DEVICE=$(networksetup -listallhardwareports | grep -A1 '^Hardware Port: Wi-Fi' | grep '^Device:' | head -1 | awk '{print $2}')
 DEVICE=${DEVICE:-en0}
+NOTIFIER_OPT="-sender io.github.tau3000.WiFiReconnect -title $APPNAME -execute : -timeout 5"
 
 if pgrep -q -f 'awakened.*networksetup'; then
-  ./terminal-notifier -sender io.github.tau3000.WiFiReconnect -title $APPNAME -message "Already running (device $DEVICE)"
+  ./terminal-notifier -message "Already running (device $DEVICE)" $NOTIFIER_OPT &
 else
-  ./awakened /bin/sh -c "$AIRPORT -I | grep -qi 'airport: off' || (networksetup -setairportpower $DEVICE off; sleep 1; networksetup -setairportpower $DEVICE on; ./terminal-notifier -sender io.github.tau3000.WiFiReconnect -title $APPNAME -message 'Wi-Fi was reconnected')" &
-  ./terminal-notifier -sender io.github.tau3000.WiFiReconnect -title $APPNAME -message "Wi-Fi will be reconnected on wakeup from sleep (device $DEVICE)"
+  ./awakened /bin/sh -c "$AIRPORT -I | grep -qi 'airport: off' || (networksetup -setairportpower $DEVICE off; sleep 1; networksetup -setairportpower $DEVICE on; ./terminal-notifier -message 'Wi-Fi was reconnected' $NOTIFIER_OPT)" &
+  ./terminal-notifier -message "Wi-Fi will be reconnected on wakeup from sleep (device $DEVICE)" $NOTIFIER_OPT &
 fi
