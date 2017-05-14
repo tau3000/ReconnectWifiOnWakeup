30ec2f2280bcf59d306d3f9797066b99b1041227
Akira Kitauchi <kitauchi@gmail.com>
2017-05-15 01:27:31 +0900

    display notification with terminal-notifiler

--- WiFiReconnect.app/Contents/MacOS/start.sh
+++ WiFiReconnect.app/Contents/MacOS/start.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-appname='WiFiReconnect'
-dir=$(cd $(dirname $0); pwd)
-airport='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
-device=$(networksetup -listallhardwareports | grep -A1 '^Hardware Port: Wi-Fi' | grep '^Device:' | head -1 | awk '{print $2}')
-device=${device:-en0}
+cd $(dirname $0)
+
+APPNAME='WiFiReconnect'
+AIRPORT='/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport'
+DEVICE=$(networksetup -listallhardwareports | grep -A1 '^Hardware Port: Wi-Fi' | grep '^Device:' | head -1 | awk '{print $2}')
+DEVICE=${DEVICE:-en0}
 
 if pgrep -q -f 'awakened.*networksetup'; then
-  osascript -e "display notification \"Already running (device $device)\" with title \"$appname\""
+  ./terminal-notifier -sender io.github.tau3000.WiFiReconnect -title $APPNAME -message "Already running (device $DEVICE)"
 else
-  "$dir/awakened" /bin/sh -c "$airport -I | grep -qi 'airport: off' || (networksetup -setairportpower $device off; sleep 1; networksetup -setairportpower $device on)" &
-  osascript -e "display notification \"Wi-Fi will be reconnected on wakeup from sleep (device $device)\" with title \"$appname\""
+  ./awakened /bin/sh -c "$AIRPORT -I | grep -qi 'airport: off' || (networksetup -setairportpower $DEVICE off; sleep 1; networksetup -setairportpower $DEVICE on; ./terminal-notifier -sender io.github.tau3000.WiFiReconnect -title $APPNAME -message 'Wi-Fi was reconnected')" &
+  ./terminal-notifier -sender io.github.tau3000.WiFiReconnect -title $APPNAME -message "Wi-Fi will be reconnected on wakeup from sleep (device $DEVICE)"
 fi
